a1e99950ae63867c03fce51d83324b0783f97b2e
careyxi <careyxi@zego.im>
2022-09-05 16:48:17 +0800

    👽: update calldemo code.

--- call/ZegoCallDemo/ZegoCallDemo/ViewController.swift
+++ call/ZegoCallDemo/ZegoCallDemo/ViewController.swift
@@ -7,12 +7,13 @@
 
 import UIKit
 import ZegoUIKitPrebuiltCall
-import ZegoUIKitSDK
 
 class ViewController: UIViewController {
     
     let selfUserID: String = String(format: "%d", Int.random(in: 0...99999))
     var selfUserName: String?
+    let yourAppID: UInt32 = 3630571287
+    let yourAppSign: String = "12a16a0cca137778fa220bf84b8af7b82ae75cddd85d7e9ca3de01d240ab1a05"
     
     @IBOutlet weak var userIDLabel: UILabel! {
         didSet {
@@ -34,13 +35,17 @@ class ViewController: UIViewController {
     @IBAction func makeNewCall(_ sender: Any) {
         
         let config: ZegoUIkitPrebuiltCallConfig = ZegoUIkitPrebuiltCallConfig()
+        let audioVideoConfig: ZegoAudioVideoViewConfig = ZegoAudioVideoViewConfig()
+        let menuBarConfig: ZegoBottomMenuBarConfig = ZegoBottomMenuBarConfig()
+        config.audioVideoViewConfig = audioVideoConfig
+        config.bottomMenuBarConfig = menuBarConfig
         let layout: ZegoLayout = ZegoLayout()
         layout.mode = .pictureInPicture
         let pipConfig: ZegoLayoutPictureInPictureConfig = ZegoLayoutPictureInPictureConfig()
         pipConfig.smallViewPostion = .topRight
         layout.config = pipConfig
         config.layout = layout
-        let callVC = ZegoUIKitPrebuiltCallVC.init(3630571287, appSign: "12a16a0cca137778fa220bf84b8af7b82ae75cddd85d7e9ca3de01d240ab1a05", userID: selfUserID, userName: self.selfUserName ?? "", callID: "009", config: config)
+        let callVC = ZegoUIKitPrebuiltCallVC.init(yourAppID, appSign: yourAppSign, userID: selfUserID, userName: self.selfUserName ?? "", callID: "100", config: config)
         callVC.modalPresentationStyle = .fullScreen
         self.present(callVC, animated: true, completion: nil)
     }
